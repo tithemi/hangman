2182a5964298b1e127ef442df29e2231906efeb2
Elizaveta Korneeva <elisavetinaaa@gmail.com>
2019-10-09 23:57:02 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,16 +1,16 @@
 import argparse
 
-from hangman.game import Game
+from hangman import Game
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dict_path', type=str, default='default_dict.csv',
                         required=False,
-                        help='Full path to *.csv file with 1 column words for game.')
+                        help='Full path to *.txt file')
     parser.add_argument('--max_tries', type=int, default=5,
                         required=False,
-                        help='The amount of wrong tries before a end of the game')
+                        help='The amount of wrong tries')
 
     args = parser.parse_args()
 
